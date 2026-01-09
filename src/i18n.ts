d91b2688bc524e30d3e570aa65284b707597e391
kaiyuan0708 <kaiyuan6953@gmail.com>
2026-01-09 14:15:46 +0800

    move translations to locales files

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,29 +1,16 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
+import en from "./locales/en/translation.json";
+import zh from "./locales/zh/translation.json";
+
 i18n.use(initReactI18next).init({
   resources: {
     en: {
-      translation: {
-        pricelist: "Price List",
-        product: "Product",
-        description: "All prices are in MYR. Postage fees are not included.",
-        shrinked: "Shrinked",
-        unshrinked: "Unshrinked",
-        noBox: "No Box",
-        loosePack: "Loose Pack",
-      },
+      translation: en,
     },
     zh: {
-      translation: {
-        pricelist: "价钱表",
-        product: "商品",
-        description: "所有价格均以 马币（MYR） 计算，邮费另计。",
-        shrinked: "原膜",
-        unshrinked: "无膜",
-        noBox: "垃圾袋",
-        loosePack: "散包",
-      },
+      translation: zh,
     },
   },
   lng: "en",
